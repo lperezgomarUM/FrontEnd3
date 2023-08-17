c429d951797d2f70f82e94a877c877169a7c561c
Lucas Pérez Gomar <135670503+lperezgomarUM@users.noreply.github.com>
2023-08-16 22:45:10 -0300

    Delete Main directory

--- Main/src/Components/Cart/Cart.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Cart = ({cart}) => {
-  return (
-    <div>{cart.map(producto => <li key={producto.id}>{producto.tipo} - {producto.precio}</li>)}</div>
-  )
-}
-
-export default Cart
--- Main/src/Components/Navbar/Navbar.styles.js
+++
@@ -1,6 +0,0 @@
-export const titleStyles = { 
-    width: 80, 
-    margin: '10px', 
-    color: 'green', 
-    backgroundColor: 'white'
-}
